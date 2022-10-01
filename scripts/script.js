a41d8d4e180ddcfcabb6caaa08c52cb1928302ee
Linda <lindathehuang@gmail.com>
2022-10-01 14:26:33 -0700

    Added functional bubble scroller

--- scripts/script.js
+++ scripts/script.js
@@ -13,33 +13,45 @@ $(document).ready(function(){
         }
     });
 
+
+
     /* Product Tags ---------------------------------------------------------------------------------- */
     
-    updateH3Tags();
-    function updateH3Tags(){
+    /* Products List */
+    fixedListScrollUpdate($(".products-items a h3"), $("#products-toggle-list"));
+
+    /* Publications List */
+    fixedListScrollUpdate($(".bubblescroller-items div h4"), $("#bubblescroller-toggle-list"));
+
+    /* Create Tags ----------------------------------------------------------------------------------- */
+    function fixedListScrollUpdate(renderItemsString, renderListString){
         let tags = [];
         let renderedtags = "";
-        let H3ID = [];
-        $(".products-items a h3").each(function(){
-            let H3TEXT = $(this).text();
-            H3ID.push( H3TEXT.replace(/ /g,'') );
-            $(this).parent().parent().attr("id", H3TEXT.replace(/ /g,'')); //Add ID to H2s
+        let HEADINGID = [];
+        renderItemsString.each(function(){
+            let HEADINGTEXT = $(this).text();
+            HEADINGID.push( HEADINGTEXT.replace(/ /g,'') );
+            $(this).parent().parent().attr("id", HEADINGTEXT.replace(/ /g,'')); //Add ID to H2s
             tags.push($(this).text());
         })
-        for(i=0; i<tags.length; i++){ renderedtags +="<li><a href='#"+H3ID[i]+"'>"+tags[i]+"</a></li>"; }
-        
-        $("#products-toggle-list").append(renderedtags);
+        for(i=0; i<tags.length; i++){ renderedtags +="<li><a href='#"+HEADINGID[i]+"'>"+tags[i]+"</a></li>"; }
+        renderListString.append(renderedtags);
     }
 
     $( window ).scroll(function() {
         $(".product-item").each(function( i ){
             let currentProduct = ".product-item:nth-child(" + (i+1) + ")";
-            linkListProducts($(currentProduct), i);
+            linkListProducts($(currentProduct), "products-toggle-list", i);
+        })
+
+        $(".bubblescroller-item").each(function( i ){
+            let currentBubbleItem = ".bubblescroller-item:nth-child(" + (i+1) + ")";
+            linkListProducts($(currentBubbleItem), "bubblescroller-toggle-list", i);
         })
     });
 
-    function linkListProducts(element, listIndex){
-        let currentListItemString = "#products-toggle-list li:nth-child(" + (listIndex + 1) + ")";
+    function linkListProducts(element, listItemID, listIndex){
+        let currentListItemString = "#" + listItemID + " li:nth-child(" + (listIndex + 1) + ")";
 
         let bounding = element.get(0).getBoundingClientRect();
         if (bounding.top >= 0 && bounding.left >= 0 && bounding.right <= (window.innerWidth || document.documentElement.clientWidth) && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
